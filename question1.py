be64e76d70d5444bf4f5fa5e312b4b729ad77610
AyimWilliam <odhisayim@gmail.com>
2023-07-20 11:20:21 +0300

    Started question

---
+++ question1.py
@@ -0,0 +1,13 @@
+def solution(S):
+    n = len(S)
+    m = len(S[0])
+
+
+# picks first word in list
+    for i in range(n):
+        #picks second word in list ensuring a word is not repeated
+        for j in range(i+1, n):
+            similar_position = {k for k in range(m)}
+
+
+    return []
